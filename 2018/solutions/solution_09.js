5b6042e5a4c38434cca0180104ae2e91b01b9e10
Gaiwecoor <gaiwecoor@gmail.com>
2018-12-10 12:28:19 -0600

    Linked marbles, per Ryn's suggestion

--- 2018/solutions/solution_09.js
+++ 2018/solutions/solution_09.js
@@ -14,28 +14,57 @@ const config = {
   line: 7
 }
 
+class Link {
+  constructor(value, newChain = false) {
+    this.value = value;
+    if (newChain) {
+      this.next = this;
+      this.previous = this;
+    }
+    return this;
+  }
+
+  insertAfter(item) {
+    if (!(item instanceof Link)) item = new Link(item);
+    item.previous = this;
+    item.next = this.next;
+    this.next.previous = item;
+    this.next = item;
+    return item;
+  }
+
+  preceding(n) {
+    return (n == 0 ? this : this.previous.preceding(n - 1));
+  }
+
+  remove() {
+    this.previous.next = this.next;
+    this.next.previous = this.previous;
+    return this;
+  }
+}
+
 const input = parse(fs.readFileSync(`${__dirname}/../input/input_09${config.test ? "_sample" : ""}.txt`, "utf8"));
 
 function play(numPlayers, last) {
   let players = [];
   for (let i = 0; i <= numPlayers; i++) players[i] = 0;
-  const circle = [0, 1];
-  let p = 1;
+  let marble = new Link(0, true);
+  marble = marble.insertAfter(1);
+
   for (let i = 2; i <= last; i++) {
     if (i % 23 != 0) {
-      p = (p + 2) % circle.length;
-      circle.splice(p, 0, i);
+      marble = marble.next.insertAfter(i);
     } else {
-      let player = i % numPlayers;
+      let player = (i % numPlayers) + 1;
       if (!players[player]) players[player] = 0;
-      p -= 7;
-      p = (p < 0 ? p += circle.length : p);
-      players[(i) % numPlayers] += i + circle.splice(p, 1)[0];
+      let removed = marble.preceding(7).remove();
+      players[i % numPlayers] += i + removed.value;
+      marble = removed.next;
     }
   }
   return Math.max(...players);
 }
-
 
 // Part 1
 function part1() {

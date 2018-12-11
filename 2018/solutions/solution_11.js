6e10815b76ba05420c915682f0718396777d2368
Gaiwecoor <gaiwecoor@gmail.com>
2018-12-11 17:14:00 -0600

    Summed-area table improvements

--- 2018/solutions/solution_11.js
+++ 2018/solutions/solution_11.js
@@ -1,5 +1,6 @@
 const SN = 5468;
 const grid = new Map();
+const sums = new Map();
 const fuelCell = new Map();
 
 var max = null;
@@ -17,32 +18,32 @@ function power(x, y) {
   }
 }
 
-function fuel(x, y, s = 3) {
-  let supply;
-  if (s == 1) supply = power(x, y);
-  else {
-    supply = fuelCell.get(`${x},${y}`);
-    for (let i = 0; i < s; i++) {
-      supply += power(x + s - 1, y + i);
-    }
-    for (let i = 0; i < s - 1; i++) {
-      supply += power(x + i, y + s - 1);
-    }
+sums.set("0,0", 0);
+sums.set("1,1", power(1, 1));
+for (let i = 2; i <= 300; i++) {
+  sums.set(`${i},0`, 0);
+  sums.set(`0,${i}`, 0);
+  sums.set(`${i},1`, sums.get(`${i - 1},1`) + power(i, 1));
+  sums.set(`1,${i}`, sums.get(`1,${i - 1}`) + power(1, i));
+}
+for (let x = 2; x <= 300; x++) {
+  for (let y = 2; y <= 300; y++) {
+    sums.set(`${x},${y}`, power(x, y) + sums.get(`${x},${y - 1}`) + sums.get(`${x - 1},${y}`) - sums.get(`${x - 1},${y - 1}`));
   }
-  fuelCell.set(`${x},${y}`, supply);
-  return supply;
+}
+
+function fuel(x, y, s = 3) {
+  return sums.get(`${x - 1},${y - 1}`) + sums.get(`${x + s - 1},${y + s - 1}`) - sums.get(`${x + s - 1},${y - 1}`) - sums.get(`${x - 1},${y + s - 1}`);
 }
 
 function part1() {
-  for (let s = 1; s <= 3; s++) {
-    for (let x = 1; x <= 301 - s; x++) {
-      for (let y = 1; y <= 301 - s; y++) {
-        let pwr = fuel(x, y, s);
-        if (s == 3 && (!max || pwr > max.pwr)) max = {pwr, x, y, s};
-      }
+  let s = 3;
+  for (let x = 1; x <= 301 - s; x++) {
+    for (let y = 1; y <= 301 - s; y++) {
+      let pwr = fuel(x, y);
+      if (!max || pwr > max.pwr) max = {pwr, x, y, s};
     }
   }
-  //console.log(max);
   return `${max.x},${max.y}`;
 }
 
@@ -55,8 +56,7 @@ function part2() {
       }
     }
   }
-  //console.log(max);
   return `${max.x},${max.y},${max.s}`;
 }
 
-module.exports = { part1, part2 };
+module.exports = { part1, part2, fuel, sums, power };

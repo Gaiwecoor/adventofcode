91a1e0fdbdb7b169e1c8292bc711e286b426d29d
Gaiwecoor <gaiwecoor@gmail.com>
2018-12-14 09:28:57 -0600

    Day 14

---
+++ 2018/solutions/solution_14.js
@@ -0,0 +1,42 @@
+const input = 540561;
+const inStr = input.toString();
+const recipes = [3, 7];
+var i, j;
+
+// Part 1
+function part1() {
+  i = 0;
+  j = 1;
+  while (recipes.length < input + 10) {
+    let sum = recipes[i] + recipes[j];
+    if (sum > 9) recipes.push(Math.floor(sum / 10));
+    recipes.push(sum % 10);
+    i += (recipes[i] + 1); i %= recipes.length;
+    j += (recipes[j] + 1); j %= recipes.length;
+  }
+  return recipes.slice(input, input + 10).join("");
+}
+
+// Part 2
+function part2() {
+  let string = recipes.join("");
+  let stringPos = 0;
+  while (string.indexOf(inStr) < 0) {
+    while (string.length > 0 && !inStr.startsWith(string)) {
+      string = string.substr(1);
+      stringPos++;
+    }
+    let sum = recipes[i] + recipes[j];
+    if (sum > 9) {
+      recipes.push(Math.floor(sum / 10));
+      string += Math.floor(sum / 10)
+    }
+    recipes.push(sum % 10);
+    string += (sum % 10);
+    i += (recipes[i] + 1); i %= recipes.length;
+    j += (recipes[j] + 1); j %= recipes.length;
+  }
+  return position = stringPos + string.indexOf(inStr);
+}
+
+module.exports = { part1, part2 }
